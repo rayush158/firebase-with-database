61035207f8d0d7986efd4391f7cc9883d350426b
Ayush Kumar <rayush158@gmail.com>
2021-12-04 23:25:10 +0530

    feat: update

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-primary text-center pt-3 pb-1 mt-5 fixed-bottom">
-      Made With ❤️
+      Made With ❤️ By Ayush
     </footer>
   );
 };
